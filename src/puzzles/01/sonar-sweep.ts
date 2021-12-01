e46ede344d0e5a41e0cc2edb0e01ad269816759e
Zachary Ho <zachyho@canva.com>
2021-12-02 09:34:32 +1100

    repo set up and done day 1

---
+++ src/puzzles/01/sonar-sweep.ts
@@ -0,0 +1,53 @@
+/** 
+* Advent of Code 2021
+* Day 1
+* Author: zachy-ho (https://github.com/zachy-ho)
+* */
+import { readFileSync } from 'fs';
+
+/** 
+* Reads a file synchronously and puts each line into an item in an array of strings 
+* @param { string } file: Path of input file
+* */
+function getArrayOfStringsFromFile(file: string) {
+  let text = readFileSync(file, 'utf-8')
+  return(text.split('\n'))
+}
+
+/** Part 1: Depth value increases */
+function getIncreasingOccurrences(records: number[] | undefined) {
+  if (!records) return;
+  let curr = records[0];
+  let occurrences = 0;
+  records.forEach((record) => {
+    if (record > curr) occurrences += 1;
+    curr = record;
+  })
+  return occurrences;
+}
+
+function runPart1() {
+  const strArr = getArrayOfStringsFromFile('./input.txt')
+  const depthValues = strArr.map((i) => parseInt(i));
+  console.log(`Part 1: ${getIncreasingOccurrences((depthValues))}`)
+}
+
+runPart1();
+
+/** Part 2: Sliding window increases */
+function getIncreasingWindows(sums: number[]) {
+  const sumArr = [];
+  for (let i = 0; i < sums.length - 2; i++) {
+    sumArr[i] = sums[i] + sums[i + 1] + sums[i + 2]
+  }
+  return sumArr;
+}
+
+function runPart2() {
+  const strArr = getArrayOfStringsFromFile('./input.txt')
+  const depthValues = strArr.map((i) => parseInt(i));
+  const sumArr = getIncreasingWindows(depthValues);
+  console.log(`Part 2: ${getIncreasingOccurrences(sumArr)}`);
+}
+
+runPart2()
