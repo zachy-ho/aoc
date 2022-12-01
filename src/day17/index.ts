8598d3f97a4ddd121bf8caf768494d58815c83ba
Zachary Ho <zyyho97@gmail.com>
2022-12-01 14:24:37 +1100

    day 17 2021

---
+++ src/day17/index.ts
@@ -0,0 +1,58 @@
+/** 
+* Advent of Code 2021
+* Day 17
+* Author: zachy-ho (https://github.com/zachy-ho)
+* */
+
+import run from "aocrunner";
+
+/* Types */
+
+/* Classes */
+
+/* Ad-hoc objects */
+
+/* Functions */
+const parseInput = (rawInput: string) => rawInput;
+
+/* Part 1: Highest possible y position */
+// NOTE: X velocity -> n(n+1)/2 has to be within (x1, x2) of the trench (consider +ve & -ve x values)
+// Y velocity -> Depending on 
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+/* Part 2: Description */
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+/* Testing & running */
+const testInput = `testInput`;
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
