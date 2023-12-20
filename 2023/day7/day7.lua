6a816ec76e5ebac4e8a969dc8984ae6097a0f67a
zachy-ho <zachyho@canva.com>
2023-12-20 21:57:59 +0800

    day 7 part 2

--- 2023/day7/day7.lua
+++ 2023/day7/day7.lua
@@ -45,7 +45,7 @@ local Type = {
 	HIGH = 1,
 }
 
-local cards = {
+local cards_part1 = {
 	["A"] = 13,
 	["K"] = 12,
 	["Q"] = 11,
@@ -63,7 +63,7 @@ local cards = {
 
 ---@param hand string
 ---@return number type Type
-local evaluate_hand = function(hand)
+local evaluate_hand_part1 = function(hand)
 	local card_counts = {}
 	local keys = {}
 	for c in string.gmatch(hand, ".") do
@@ -100,11 +100,12 @@ local evaluate_hand = function(hand)
 end
 
 ---@param sets table
+---@param evaluate_fn function
 ---@return table buckets
-local put_into_buckets = function(sets)
+local put_into_buckets = function(sets, evaluate_fn)
 	local buckets = {}
 	for _, set in ipairs(sets) do
-		local hand = evaluate_hand(set.hand)
+		local hand = evaluate_fn(set.hand)
 		if buckets[hand] == nil then
 			buckets[hand] = { set }
 		else
@@ -117,7 +118,7 @@ end
 ---@param hand1 string
 ---@param hand2 string
 ---@return boolean | nil hand1_higher_than_hand2
-local is_lower = function(hand1, hand2)
+local is_lower = function(hand1, hand2, cards)
 	local cards1 = {}
 	local cards2 = {}
 	for c in string.gmatch(hand1, ".") do
@@ -138,19 +139,19 @@ local is_lower = function(hand1, hand2)
 	return nil
 end
 
----@param bucket table { { hand, bid } }
----@return table sorted_bucket
-local sort = function(bucket)
-	-- P(bucket)
-	table.sort(bucket, function(a, b)
-		return is_lower(a.hand, b.hand)
-	end)
-	return bucket
-end
-
 local part_1 = function()
 	local sets = parse_sets()
-	local buckets = put_into_buckets(sets)
+	local buckets = put_into_buckets(sets, evaluate_hand_part1)
+
+	---@param bucket table { { hand, bid } }
+	---@return table sorted_bucket
+	local sort = function(bucket)
+		table.sort(bucket, function(a, b)
+			return is_lower(a.hand, b.hand, cards_part1)
+		end)
+		return bucket
+	end
+
 	local bucket_keys = {}
 	for k, bucket in pairs(buckets) do
 		sort(bucket)
@@ -167,9 +168,6 @@ local part_1 = function()
 		local bucket = buckets[k]
 		for _, set in pairs(bucket) do
 			local score = set.bid * rank
-			if set.hand == "22228" then
-				P(rank)
-			end
 			sum = sum + score
 			rank = rank + 1
 		end
@@ -177,9 +175,112 @@ local part_1 = function()
 	return sum
 end
 
-local part_2 = function() end
+local cards_part2 = {
+	["A"] = 13,
+	["K"] = 12,
+	["Q"] = 11,
+	["T"] = 10,
+	["9"] = 9,
+	["8"] = 8,
+	["7"] = 7,
+	["6"] = 6,
+	["5"] = 5,
+	["4"] = 4,
+	["3"] = 3,
+	["2"] = 2,
+	["J"] = 1,
+}
+
+---@param hand string
+---@return number type Type
+local evaluate_hand_part2 = function(hand)
+	local card_counts = {}
+	local keys = {}
+	for c in string.gmatch(hand, ".") do
+		if card_counts[c] == nil then
+			card_counts[c] = 1
+			table.insert(keys, c)
+		else
+			card_counts[c] = card_counts[c] + 1
+		end
+	end
+	table.sort(keys, function(a, b)
+		return card_counts[a] > card_counts[b]
+	end)
+
+	-- if J exists, fit it in
+	if card_counts["J"] ~= nil then
+		if size(keys) == 1 or size(keys) == 2 then
+			return Type.FIVE
+		elseif size(keys) == 3 then
+			if card_counts[keys[1]] == card_counts[keys[2]] and keys[3] == "J" then
+				return Type.HOUSE
+			else
+				return Type.FOUR
+			end
+		elseif size(keys) == 4 then
+			return Type.THREE
+		else
+			return Type.ONE
+		end
+	end
+
+	if card_counts[keys[1]] == 5 then
+		return Type.FIVE
+	elseif card_counts[keys[1]] == 4 then
+		return Type.FOUR
+	elseif card_counts[keys[1]] == 3 then
+		if card_counts[keys[2]] == 2 then
+			return Type.HOUSE
+		else
+			return Type.THREE
+		end
+	elseif card_counts[keys[1]] == 2 then
+		if card_counts[keys[2]] == 2 then
+			return Type.TWO
+		else
+			return Type.ONE
+		end
+	else
+		return Type.HIGH
+	end
+end
+
+local part_2 = function()
+	local sets = parse_sets()
+	local buckets = put_into_buckets(sets, evaluate_hand_part2)
+
+	---@param bucket table { { hand, bid } }
+	---@return table sorted_bucket
+	local sort = function(bucket)
+		table.sort(bucket, function(a, b)
+			return is_lower(a.hand, b.hand, cards_part2)
+		end)
+		return bucket
+	end
+
+	local bucket_keys = {}
+	for k, bucket in pairs(buckets) do
+		sort(bucket)
+		table.insert(bucket_keys, k)
+	end
+
+	table.sort(bucket_keys, function(a, b)
+		return tonumber(a) < tonumber(b)
+	end)
+
+	local rank = 1
+	local sum = 0
+	for _, k in ipairs(bucket_keys) do
+		local bucket = buckets[k]
+		for _, set in pairs(bucket) do
+			local score = set.bid * rank
+			sum = sum + score
+			rank = rank + 1
+		end
+	end
+	return sum
+end
 
 print(part_1())
 print(part_2())
-
--- 251213677 (wrong)
