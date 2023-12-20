e7d8d4e3663f68d4120b663caedb6763c7f29c5a
zachy-ho <zachyho@canva.com>
2023-12-20 22:00:47 +0800

    made day 8

---
+++ 2023/day8/day8.lua
@@ -0,0 +1,28 @@
+-- Just so I can require("utils") without worrying about where I'm executing this file from
+local current_directory = debug.getinfo(1, "S").source:match([[^@?(.*[\/])[^\/]-$]])
+local path = ";" .. current_directory .. "?.lua"
+if not string.match(package.path, ";" .. current_directory .. "%?.lua") then
+	package.path = package.path .. path
+end
+
+local utils = require("utils")
+
+local parse = function()
+	local script_path = debug.getinfo(1, "S").source:sub(2)
+	local script_directory = script_path:match("(.*/)")
+	local file = assert(io.open(script_directory .. "input.txt", "r"))
+	local input = {}
+	for l in file:lines() do
+		table.insert(input, l)
+	end
+	file:close()
+	return input
+end
+local input = parse()
+
+local part_1 = function() end
+
+local part_2 = function() end
+
+print(part_1())
+print(part_2())
